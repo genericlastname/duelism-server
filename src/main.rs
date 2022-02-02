d2d4ad48b769d3d2617d8a6b8a56f65ec344b770
Kyle Brown <brownkylej@gmail.com>
2022-02-02 11:42:58 -0500

    Reqwest test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+
+#[tokio::main]
+async fn main() {
+    let result =
+        reqwest::get("https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Tornado%20Dragon").await;
+    println!("{:?}", result);
 }
