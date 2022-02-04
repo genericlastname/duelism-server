66132c41cbf6a09418b7dfbf8584bf3f1262ed86
Kyle Brown <brownkylej@gmail.com>
2022-02-04 15:23:39 -0500

    Start db code

--- src/carddb.rs
+++ src/carddb.rs
@@ -1,14 +1,22 @@
+use rusqlite::{Connection, params};
 use serde::{Deserialize, Serialize};
-
-// #[derive(Serialize, Deserialize, Debug)]
-// enum Card {
-//     MonsterCard(MonsterCard),
-//     SpellTrapCard(SpellTrapCard),
-// }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct MonsterCardCollection {
     data: Vec<MonsterCard>,
+}
+
+impl MonsterCardCollection {
+    pub fn insert_to_db(&self, conn: &Connection) {
+        for mc in &self.data {
+            conn.execute(
+                "INSERT INTO monsters (name, kind, desc, atk, def, level, race)
+                VALUES
+                    (?1, ?2, ?3, ?4, ?5, ?6, ?7)",
+                params![mc.name, mc.kind, mc.desc, mc.atk, mc.def, mc.level, mc.race]
+            ).unwrap();
+        } 
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -45,4 +53,20 @@ struct CardImages {
     id: i32,
     image_url: String,
     image_url_small: String,
+}
+
+pub fn setup_db(conn: &Connection) {
+    conn.execute(
+        "CREATE TABLE monsters (
+            id      INT PRIMARY KEY,
+            name    TEXT NOT NULL,
+            kind    TEXT,
+            desc    TEXT,
+            atk     INT,
+            def     INT,
+            level   INT,
+            race    TEXT
+        )",
+        [],
+    ).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use reqwest;
+use rusqlite::Connection;
 
 mod carddb;
 
@@ -11,13 +12,28 @@ async fn main() {
         .await
         .unwrap();
 
+    let coll: carddb::MonsterCardCollection;
     match response.status() {
         reqwest::StatusCode::OK => {
-            match response.json::<carddb::MonsterCardCollection>().await {
-                Ok(parsed) => println!("{:?}", parsed),
-                Err(e) => println!("Err: {:?}", e),
+            coll = match response.json::<carddb::MonsterCardCollection>().await {
+                Ok(parsed) => parsed,
+                Err(e) => panic!("Err {:?}", e),
             }
         }
         _ => { panic!("Uh oh"); }
     }
+
+    // let conn = Connection::open_in_memory().unwrap();
+    let conn = Connection::open("test.db3").unwrap();
+    carddb::setup_db(&conn);
+    coll.insert_to_db(&conn);
+    // let mut stmt = conn.prepare("SELECT name, desc, atk, def").unwrap();
+    // let m_iter = stmt.query_map([], |row| {
+    //     Ok(format!("{}\n{}\natk: {}\tdef: {}\n",
+    //             row.get(0).unwrap(),
+    //             row.get(1).unwrap(),
+    //             row.get(2).unwrap(),
+    //             row.get(3).unwrap())
+    //     )
+    // }).unwrap();
 }
